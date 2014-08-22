781f577fbc55409ed398bcb466bf11195283819c
Gustavo Pelaez Restrepo <gustavopelaezrpo@gmail.com>
2014-08-22 17:11:22 -0500

    estilos

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers', [])
-.controller('MainCtrl', function($scope, $ionicModal, $ionicPopup) {
+.controller('MainCtrl', function($scope, $ionicModal, $ionicPopup, $timeout) {
 	$ionicModal.fromTemplateUrl('views/modal-config.html', {
 		scope: $scope,
 		animation: 'slide-in-up'
@@ -10,6 +10,7 @@ angular.module('starter.controllers', [])
 	if(!settings) settings = {
 		title: 'Configure la aplicación',
 		backgroundColor: '#333',
+		textColor: '#ddd',
 		password: '0000',
 	}
 	$scope.settings = settings;
@@ -37,6 +38,18 @@ angular.module('starter.controllers', [])
 		} catch(e) {
 			console.warn(e.message);
 			$scope.modalConfig.show();
+		}
+	}
+	$scope.initGallery = function(index) {
+		var gallery = $scope.settings.gallery[index];
+		gallery.thumbnail = (gallery.images.length>0? gallery.images[0]:null);
+		if(gallery.images.length>1) {
+			var currentThumbnail = 0;
+			$timeout(function() {
+				currentThumbnail++;
+				if(currentThumbnail>gallery.images.length) currentThumbnail = 0;
+				$scope.settings.gallery[index].thumbnail 
+			}, 1400)
 		}
 	}
 })
